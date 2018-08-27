2c21991c41628abe124b929170b2e363bb774758
Freddie Sanchez <freddie.sanchez@gmail.com>
2018-08-26 22:43:01 -0700

    FPiS - Chapter 4 and Chatper 5 - Complete 4, started 5

--- src/main/scala/fpis/Chapter4.Either.scala
+++ src/main/scala/fpis/Chapter4.Either.scala
@@ -1,3 +1,5 @@
+package fpis.Chapter4
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
@@ -24,29 +26,36 @@ trait Either[+E, +A] {
     }
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = 
-  /*  for {
+    for {
       a <- this
       bb <- b
     } yield f(a,bb)
-  */
-  this flatMap(a => b.map(bb => f(a,bb)))
+
+
+
+}
+
+object Either {
 
 
   // EXERCISE 4.7
   // Implement sequence and traverse for Either . These should return the first error
   // that’s encountered, if there is one.
   def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = 
-    res match {
+    es match {
       case Nil => Right(Nil)
-      case x::xs => x.flatMap((y:Either[E,A]) => sequence(xs).map((z:A) => z::_))
+      case x::xs => x.flatMap((y:A) => sequence(xs).map((z:List[A]) => y::z))
     }
 
-  def traverse[E, A, B](as: List[A])(
-    f: A => Either[E, B]): Either[E, List[B]]
+  def traverse[E, A, B](as: List[A])( f: A => Either[E, B]): Either[E, List[B]] =
+    sequence(as map f)
 
-}
-
-object Either {
-
+  // EXERCISE 4.8
+  // In this implementation, map2 is only able to report one error, even if both the name
+  // and the age are invalid. What would you need to change in order to report both errors?
+  // Would you change map2 or the signature of mkPerson ? Or could you create a new data
+  // type that captures this requirement better than Either does, with some additional
+  // structure? How would orElse , traverse , and sequence behave differently for that
+  // data type?
 
 }
